ff9dad8f5018f1ff3b7c03ab8207ed2b5d797dbe
SeoungMinHwang <117965731+SeoungMinHwang@users.noreply.github.com>
2024-05-08 21:35:21 +0900

    feat: ReviewView.vue

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,15 @@
 <script setup>
 import MainView from './views/MainView.vue'
 import HeaderView from './views/HeaderView.vue'
+import ReviewView from './views/ReviewView.vue';
+
 </script>
 
 <template>
   <div>
     <HeaderView />
     <MainView />
+    <ReviewView />
   </div>
 </template>
 
