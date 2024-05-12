019960974f156928de3bcde1517b052701fcfbda
lij0825 <dldlswns890@gmail.com>
2024-05-12 15:29:30 +0900

    feat : preline 삭제

--- src/main.js
+++ src/main.js
@@ -4,7 +4,6 @@ import { useKakao } from 'vue3-kakao-maps/@utils'
 import './stylesheet/index.css'
 import App from './App.vue'
 import router from './router'
-import 'preline/preline'
 
 useKakao('c9c4977a5d4ddc19f25aa9b22c1e264a')
 
