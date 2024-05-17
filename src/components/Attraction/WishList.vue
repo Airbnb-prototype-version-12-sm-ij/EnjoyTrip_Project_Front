c3aae188a67bc724f1ca08e757d4769bb82ffd6b
lij0825 <dldlswns890@gmail.com>
2024-05-17 23:38:43 +0900

    feat : 흠쓰

--- src/components/Attraction/WishList.vue
+++ src/components/Attraction/WishList.vue
@@ -3,9 +3,9 @@ import client from '@/api/client'
 import MarkdownIt from 'markdown-it'
 import WishListItem from './WishListItem.vue'
 import { ref, onMounted } from 'vue'
-import WishMap from './WishMap.vue'
 import OpenAI from 'openai'
-import Draggable from "vue3-draggable";
+import Draggable from 'vue3-draggable'
+import KakaoMobility from './KakaoMobility.vue'
 
 const markdown = new MarkdownIt()
 
@@ -23,10 +23,7 @@ const getWishList = async () => {
   isInit.value = true
 }
 
-
 let text = ref('')
-
-
 
 const isLoading = ref(false)
 
@@ -34,7 +31,6 @@ const openai = new OpenAI({
   apiKey: import.meta.env.VITE_OPENAI_API_KEY,
   dangerouslyAllowBrowser: true
 })
-
 
 const result = ref({
   content: ''
@@ -91,17 +87,13 @@ const getExplanation = async () => {
   result.value = response.choices[0].message
 }
 
-
 onMounted(async () => {
   await getWishList()
 })
 </script>
 
 <template>
-
-
   <div class="flex">
-
     <div class="mt-[100px] col-span-3" v-if="wishList">
       <h1 style="margin-left: 300px; font-size: 36px">찜 목록</h1>
       <!-- <WishListItem :wish="wish" v-for="wish in wishList" :key="wish.id" />
@@ -115,22 +107,28 @@ onMounted(async () => {
       </draggable>
     </div>
 
-
-
     <div class="flex-col">
-      <WishMap :wishList="wishList" v-if="wishList" />
+      <KakaoMobility />
 
       <div class="flex items-start justify-start h-3/4 bg-gray-900 max-w-[1000px]">
         <div class="flex flex-col">
-          <div @click="getTripRoute" style="width: 300px"
-            class="flex mt-5 ml-5 px-6 py-3 rounded-md bg-gray-50 text-gray-900 font-medium hover:bg-gray-200 focus:outline-none focus:ring-2 focus:ring-gray-500 focus:ring-offset-2">
+          <div
+            @click="getTripRoute"
+            style="width: 300px"
+            class="flex mt-5 ml-5 px-6 py-3 rounded-md bg-gray-50 text-gray-900 font-medium hover:bg-gray-200 focus:outline-none focus:ring-2 focus:ring-gray-500 focus:ring-offset-2"
+          >
             <img src="@/assets/GPT.png" width="60px" />
             <a class="flex items-center justify-center ml-5">
               여행경로 추천 받기 {{ isLoading }}
             </a>
           </div>
 
-          <div role="status" class="max-w-2xl animate-pulse mx-10 mt-20" style="width: 1500px" v-show="isLoading">
+          <div
+            role="status"
+            class="max-w-2xl animate-pulse mx-10 mt-20"
+            style="width: 1500px"
+            v-show="isLoading"
+          >
             <div class="h-2.5 bg-gray-200 rounded-full dark:bg-gray-700 w-340 mb-4"></div>
             <div class="h-2.5 bg-gray-200 rounded-full dark:bg-gray-700 max-w-[360px] mb-4"></div>
             <div class="h-2.5 bg-gray-200 rounded-full dark:bg-gray-700 w-340 mb-4"></div>
@@ -149,7 +147,6 @@ onMounted(async () => {
           <p v-html="markdown.render(result.content)" style="color: white" class="ml-14 mt-14"></p>
         </div>
       </div>
-
     </div>
   </div>
 </template>
