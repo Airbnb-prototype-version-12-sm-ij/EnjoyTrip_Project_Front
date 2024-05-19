04ea74e05ab5ca557e8319290a01041b6cc584ea
lij0825 <dldlswns890@gmail.com>
2024-05-19 17:19:15 +0900

    feat : 관광지 디테일 페이지 수정

--- src/components/common/Score.vue
+++ src/components/common/Score.vue
@@ -1,20 +1,18 @@
 <script setup>
-
 const props = defineProps({
-    score: Number
+  score: Number
 })
-
-
 </script>
 
 <template>
-    <div class="flex space-x-1">
-        <div v-for="i in 5" :key="i" class="w-5 h-5 rounded-full"
-            :class="{ 'bg-[#00af87]': i <= score, 'bg-gray-300': i > score }"></div>
-    </div>
+  <div class="flex space-x-1">
+    <div
+      v-for="i in 5"
+      :key="i"
+      class="w-5 h-5 rounded-full"
+      :class="{ 'bg-[#00af87]': i <= score, 'bg-gray-300': i > score }"
+    ></div>
+  </div>
 </template>
-
-
-
 
 <style scoped></style>
