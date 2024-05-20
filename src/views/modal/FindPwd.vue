f2819178c4e38c4babeda3e11a24ee2d7d649476
SeoungMinHwang <hsm990423@gmail.com>
2024-05-20 15:01:55 +0900

    feat : 비번찾기

--- src/views/modal/FindPwd.vue
+++ src/views/modal/FindPwd.vue
@@ -2,9 +2,9 @@
 import { ref } from 'vue'
 import { useRouter } from 'vue-router'
 import Swal from 'sweetalert2'
-// import client from '@/api/client'
+import client from '@/api/client'
 
-// const router = useRouter()
+const router = useRouter()
 
 // 알림창
 const Toast = Swal.mixin({
@@ -19,54 +19,88 @@ const Toast = Swal.mixin({
   }
 })
 
-const userId = ref('')
+const idForFind = ref('')
 const userEmail = ref('')
+
+
+const findpwd = async (e) => {
+
+  e.preventDefault()
+  try {
+    const response = await client.post('/members/find', {
+      userId: idForFind.value,
+      userEmail: userEmail.value
+    })
+
+    if (response.status !== 200) {
+      Toast.fire({
+        icon: 'error',
+        title: '입력 양식을 확인하세요.'
+      })
+    }
+
+    Swal.fire({
+      title: '임시 비밀번호 발송 성공',
+      icon: 'success',
+      confirmButtonColor: '#3085d6',
+      confirmButtonText: '확인'
+    }).then(() => {
+      router.go(0)
+    })
+  } catch (error) {
+    console.error('에러' + error)
+    Toast.fire({
+      icon: 'error',
+      title: error
+    })
+  }
+
+}
+// client.post('/findpwd', {
+//   id: idForFind.value,
+//   email: userEmail.value
+// }).then((res) => {
+//   if (res.data === 'success') {
+//     Toast.fire({
+//       icon: 'success',
+//       title: 'Password has been sent to your email'
+//     })
+//     router.push('/')
+//   } else {
+//     Toast.fire({
+//       icon: 'error',
+//       title: 'Please check your ID and Email'
+//     })
+//   }
+// }).catch((error) => {
+//   console.error('에러' + error)
+// })
+// }
+
+
 </script>
 
 <template>
-  <a
-    data-modal-hide="login-modal"
-    data-modal-target="findpwd-modal"
-    data-modal-toggle="findpwd-modal"
-    type="button"
-    class="text-sm text-blue-700 hover:underline dark:text-blue-500"
-  >
+  <a data-modal-hide="login-modal" data-modal-target="findpwd-modal" data-modal-toggle="findpwd-modal" type="button"
+    class="text-sm text-blue-700 hover:underline dark:text-blue-500">
     Find Password
   </a>
   <teleport to="body">
-    <div
-      id="findpwd-modal"
-      tabindex="-1"
-      aria-hidden="true"
-      class="hidden overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full min-h-full"
-    >
+    <div id="findpwd-modal" tabindex="-1" aria-hidden="true"
+      class="hidden overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full min-h-full">
       <div class="relative p-4 w-full max-w-md max-h-full">
         <!-- Modal content -->
         <div class="relative bg-white rounded-lg shadow dark:bg-gray-700">
           <!-- Modal header -->
-          <div
-            class="flex items-center justify-between p-4 md:p-5 border-b rounded-t dark:border-gray-600"
-          >
+          <div class="flex items-center justify-between p-4 md:p-5 border-b rounded-t dark:border-gray-600">
             <h3 class="text-xl font-semibold text-gray-900 dark:text-white">Find Password</h3>
-            <button
-              type="button"
+            <button type="button"
               class="end-2.5 text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white"
-              data-modal-hide="findpwd-modal"
-            >
-              <svg
-                class="w-3 h-3"
-                aria-hidden="true"
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 14 14"
-              >
-                <path
-                  stroke="currentColor"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  stroke-width="2"
-                  d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"
-                />
+              data-modal-hide="findpwd-modal">
+              <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none"
+                viewBox="0 0 14 14">
+                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                  d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6" />
               </svg>
               <span class="sr-only">Close modal</span>
             </button>
@@ -75,34 +109,19 @@ const userEmail = ref('')
             <form class="space-y-4" action="#">
               <div>
                 <!-- 아이디 입력 -->
-                <input
-                  v-model="userId"
-                  type="userId"
-                  name="userId"
-                  id="new-userId"
-                  class="text-sm rounded-lg block w-full p-2.5 bg-green-50 border border-green-500 text-green-900 dark:text-green-400 placeholder-green-700 dark:placeholder-green-500 focus:ring-green-500 focus:border-green-500 dark:bg-gray-700 dark:border-green-500"
-                  placeholder="ID"
-                  required
-                />
+                <input v-model="idForFind" type="idForFind" name="idForFind" id="idForFind"
+                  class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"
+                  placeholder="ID" required />
               </div>
 
               <div>
                 <!-- 이메일 입력 -->
-                <input
-                  v-model="userEmail"
-                  type="email"
-                  name="userEmail"
-                  id="userEmail"
-                  placeholder="Email"
-                  class="text-sm rounded-lg block w-full p-2.5 bg-green-50 border border-green-500 text-green-900 dark:text-green-400 placeholder-green-700 dark:placeholder-green-500 focus:ring-green-500 focus:border-green-500 dark:bg-gray-700 dark:border-green-500"
-                  required
-                />
+                <input v-model="userEmail" type="email" name="userEmail" id="userEmail" placeholder="Email"
+                  class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"
+                  required />
               </div>
-              <button
-                @click="findpwd"
-                type="submit"
-                class="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-              >
+              <button @click="findpwd" type="submit"
+                class="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
                 FindPassword
               </button>
             </form>
--- src/views/modal/SignupModal.vue
+++ src/views/modal/SignupModal.vue
@@ -157,7 +157,8 @@ const signup = async (e) => {
 
 
               <!-- 이메일 입력 -->
-              <input v-model="email" type="email" name="email" id="email" class='text-sm rounded-lg block w-full p-2.5'
+              <input v-model="email" type="email" name="email" id="email"
+                class='bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white'
                 placeholder="E-mail" required />
 
             </div>
