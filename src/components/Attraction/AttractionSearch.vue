7434ddd838eb19e3275d0077e8d7990b98fb3d02
lij0825 <lij0825@gmail.com>
2024-05-21 12:13:41 +0900

    feat : 색 골랐음

--- src/components/Attraction/AttractionSearch.vue
+++ src/components/Attraction/AttractionSearch.vue
@@ -421,7 +421,7 @@ const attractionSearch = async (e) => {
         />
         <button
           type="submit"
-          class="absolute top-0 end-0 p-2.5 text-sm font-medium h-full text-white bg-blue-700 rounded-e-lg border border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+          class="absolute top-0 end-0 p-2.5 text-sm font-medium h-full text-white bg-mycolor rounded-e-lg border border-mycolor hover:bg-mycolor-hover focus:ring-4 focus:outline-none focus:ring-mycolor"
         >
           <svg
             class="w-4 h-4"
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,10 @@
 export default {
   content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}', './node_modules/flowbite/**/*.js'],
   theme: {
+    colors: {
+      mycolor: '#83b5d1',
+      'mycolor-hover': '#7698b3'
+    },
     extend: {}
   },
   plugins: [require('flowbite/plugin')]
