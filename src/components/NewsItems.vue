ac06001fd6979e39517239b9a390ee1152b98921
lij0825 <lij0825@gmail.com>
2024-05-21 17:20:07 +0900

    fix : 뉴스 부분 수정

--- src/components/NewsItems.vue
+++ src/components/NewsItems.vue
@@ -28,27 +28,6 @@ const openLink = () => {
       <div class="flex justify-between">
         <p class="text-gray-500 dark:text-gray-400 mt-2">{{ news.title }}</p>
       </div>
-      <div class="flex items-center justify-between mt-4">
-        <div class="flex items-center space-x-2">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="24"
-            height="24"
-            viewBox="0 0 24 24"
-            fill="none"
-            stroke="currentColor"
-            stroke-width="2"
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            class="w-4 h-4 text-gray-500 dark:text-gray-400"
-          >
-            <path
-              d="M19 14c1.49-1.46 3-3.21 3-5.5A5.5 5.5 0 0 0 16.5 3c-1.76 0-3 .5-4.5 2-1.5-1.5-2.74-2-4.5-2A5.5 5.5 0 0 0 2 8.5c0 2.3 1.5 4.05 3 5.5l7 7Z"
-            ></path>
-          </svg>
-          <span class="text-gray-500 dark:text-gray-400 text-sm">25 likes</span>
-        </div>
-      </div>
     </div>
   </div>
 </template>
